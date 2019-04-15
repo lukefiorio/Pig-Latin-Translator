57d9c0f59288b0b2c8e4a1521b7d19d2f28c94c4
Luke Fiorio <lukefiorio@Lukes-MBP.home>
2019-04-15 10:51:08 -1000

    added pigLatinToEnglish function

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 function englishToPigLagin(word) {
+  if (typeof word !== 'string') {
+    console.log('Must be a string!');
+    return;
+  }
+
   if (word.split(' ')[0] === '') {
     console.log(`Don't start with an empty space, please!`);
     return;
@@ -58,4 +63,46 @@ function englishToPigLagin(word) {
     charArr.push(`-${strToMove}ay`);
     return charArr.join('');
   }
+}
+
+function pigLatinToEnglish(word) {
+  // require a string
+  if (typeof word !== 'string') {
+    console.log('Must be a string!');
+    return;
+  }
+
+  // require no leading space
+  if (word.split(' ')[0] === '') {
+    console.log(`Don't start with an empty space, please!`);
+    return;
+  }
+
+  // require single word
+  if (word.split(' ').length > 1) {
+    console.log('One word at a time, please!');
+    return;
+  }
+
+  // require a hyphen in our pig latin word
+  const charArr = [...word];
+  let foundHyphen = 0;
+  for (let i = 0; i < charArr.length; i++) {
+    if (['-'].includes(charArr[i])) {
+      foundHyphen = 1;
+    }
+  }
+  if (!foundHyphen) {
+    console.log(`Not a word in pig latin! (no hyphen '-')`);
+    return;
+  }
+
+  // translate to english
+  const startMoveUpIndex = word.lastIndexOf('-') + 1;
+  const endMoveUpIndex = word.lastIndexOf('a');
+  const startEnglish = word.substr(startMoveUpIndex, endMoveUpIndex - startMoveUpIndex);
+  const endEnligsh = word.substr(0, startMoveUpIndex - 1);
+  const english = startEnglish + endEnligsh;
+
+  return english;
 }
